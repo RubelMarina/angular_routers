270464fe2acb3f55c30029f0dbc90bcf4c9688f7
MaRina <marina88rubel@gmail.com>
2021-08-27 15:09:27 +0300

    #1

---
+++ src/app/phrases/phrase-home/phrase-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-phrase-home',
+  templateUrl: './phrase-home.component.html',
+  styleUrls: ['./phrase-home.component.css']
+})
+export class PhraseHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
